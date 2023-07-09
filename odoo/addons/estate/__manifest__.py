1a9924bdfc5ea6ffb3b052cdc88f0261ac48db86
Abdulaziz-khlid <azzozy49@icloud.com>
2023-07-09 03:03:44 +0300

    add some security

--- odoo/addons/estate/__manifest__.py
+++ odoo/addons/estate/__manifest__.py
@@ -1,7 +1,12 @@
 {
     'name': 'Real Estate',
+    'license': 'LGPL-3',
     'depends': [
         'base',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
     ],
     'installable': True,
     'application': True,
