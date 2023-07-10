0feb044c55b6bc82e0e46847519f62ecfa8d1f53
Turki72 <Tourkix33@gmail.com>
2023-07-10 15:58:18 +0300

    chapter-6_turki

--- addons/estate/models/property.py
+++ addons/estate/models/property.py
@@ -1,13 +1,17 @@
-from odoo import fields,models
+from datetime import date, timedelta
+from odoo import fields, models
 
-class estate_turki(models.Model):
-    _name = "estate.property"
-    Name=fields.Char('title',required=True)
-    Description=fields.Text('Description',required=False)
-    Postcode=fields.Char('Postcode',required=True)
-    Date_availability=fields.Date('Availability From',required=False)
-    Expected_Price=fields.Float('Expected Price',required=True)
-    selling_price = fields.Float('Selling Price', required=False, default=0.0)
+
+class RealEstateProperty(models.Model):
+    _name = 'estate.property'
+    _description = 'Real Estate Properties'
+    name = fields.Char('Title', required=True)
+    active = fields.Boolean('Active', default=False)
+    description = fields.Text('Description', required=False)
+    postcode = fields.Char('Postcode', required=True)
+    date_availability = fields.Date('Available From', required=False, copy=False, default=lambda self: date.today()+timedelta(days=90))
+    expected_price = fields.Float('Expected Price', required=True)
+    selling_price = fields.Float('Selling Price', required=False, default=0.0, readonly=True)
     bedrooms = fields.Integer('Bedrooms', default=1)
     living_area = fields.Integer('Total Area (sqm)', required=False)
     facades = fields.Integer('Facades',default=0)
@@ -17,3 +21,8 @@ class estate_turki(models.Model):
     garden_orientation = fields.Selection(
         [('N', 'North'), ('S','South'),('E','East'),('W','West')], 
         'Garden Orientation', required=False)
+    state = fields.Selection([('N', 'New'), ('R', 'Offer Received'), 
+                              ('A', 'Offer Accepted'), ('S', 'Sold'), 
+                              ('C', 'Canceled')], 'State', required=True, 
+                              copy=False, default='N')
+
